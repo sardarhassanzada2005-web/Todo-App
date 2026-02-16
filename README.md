ea896cf5225a30f644f197f57bec8374bbdedb64
Sardar <sardarhassanzada2005@gmail.com>
2026-02-16 12:43:28 -0800

    Complete JavaScript logic for todoApp Add reset feature and refactor code ZZ

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# 📝 Todo App( JavaScript ) 
+
+>### click [_here_](https://github.com/sardarhassanzada2005-web/Todo-App) to see demo
+
+
+
+### Overview
+    This is a simple Todo application built to practice JavaScript fundamentals, especially DOM manipulation and basic CRUD operation.
+
+###  Features
+    - Add, edit, delete tasks
+    - Mark tasks as completed
+    - Save tasks using LocalStorage so data is not lost on refresh
+### Technologies
+    - HTML
+    - CSS
+    - JavaScript
+### What I Learned
+    - How to select and update  DOM elements using JavaScript
+    - How to Organize JavaScript code with functions
+    - How to store and retrieve data using LocalStorage
+
+_This project will be improved step by step as I continue learnig._
+
+
--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
 const todoListUL = document.getElementById('todo-list');
 
-let allTodos = [];
+let allTodos = getTodos();
+updateTodoList();
 
 todoForm.addEventListener('submit' , function(e){
     e.preventDefault();
@@ -13,12 +14,73 @@ todoForm.addEventListener('submit' , function(e){
 function addTodo(){
     const todoText = todoInput.value.trim();
     if(todoText.length > 0){
-        allTodos.push(todoText);
-        createTodoItem(todoText);
+        const todoObject = {
+            text: todoText,
+            completed: false
+        }
+        allTodos.push(todoObject);
+        updateTodoList()
+        saveTodos();
         todoInput.value = "";
     }
 }
 
-function createTodoItem(todo){
-    
+function updateTodoList(){
+    todoListUL.innerHTML = "";
+    allTodos.forEach((todo, todoIndex)=>{
+        todoItem = createTodoItem(todo, todoIndex);
+        todoListUL.append(todoItem);
+    })
 }
+
+function createTodoItem(todo, todoIndex){
+    const todoId = "todo-"+todoIndex;
+    const todoLI = document.createElement("li");
+    const todoText = todo.text;
+    todoLI.className = "todo";
+    todoLI.innerHTML = `
+
+        <input type="checkbox" id="${todoId}">
+        <label class="custom-checkbox" for="${todoId}">
+            <img src="icons/check_small_30dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg" alt="done icon" fill="transparent">
+        </label>
+        <label class="todo-text" for="${todoId}" >
+            ${todoText}
+        </label>
+        <button class="delete-btn">
+            <img  src="icons/delete_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg" alt="delete btn">
+        </button>
+    
+       `
+    const deleteButton = todoLI.querySelector(".delete-btn");
+    deleteButton.addEventListener("click" , ()=>{
+        deleteTodoItem(todoIndex);
+    })
+    
+    const checkbox = todoLI.querySelector("input");
+    checkbox.addEventListener("change", ()=>{
+        allTodos[todoIndex].completed = checkbox.checked;
+        saveTodos();
+    })
+
+    checkbox.checked = todo.completed;
+
+    return todoLI;
+}
+
+//      localStorage
+
+function deleteTodoItem (todoIndex){
+    allTodos = allTodos.filter((_, i)=> i !== todoIndex);
+    saveTodos();
+    updateTodoList();
+}
+function saveTodos(){
+    const todoJson = JSON.stringify(allTodos);
+    localStorage.setItem("todos", todoJson)
+}
+function getTodos(){
+    const todos = localStorage.getItem("todos") || "[]";
+    return JSON.parse(todos);
+}
+
