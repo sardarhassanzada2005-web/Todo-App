4049b87cf3706e5f38390b4bb8690139a1e98886
Sardar <sardarhassanzada2005@gmail.com>
2026-02-17 11:59:36 -0800

    fixed icons

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function createTodoItem(todo, todoIndex){
             ${todoText}
         </label>
         <button class="delete-btn">
-            <img  src="icons/delete_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg" alt="delete btn">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#4a4d57"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
         </button>
     
        `
